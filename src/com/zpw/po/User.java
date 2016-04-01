2b4d1df4f407c97be978a79abadeda877ca926bd
HuChuan <691523638@qq.com>
2016-04-01 19:03:07 +0800

    加private

--- src/com/zpw/po/User.java
+++ src/com/zpw/po/User.java
@@ -3,16 +3,16 @@ package com.zpw.po;
 import java.sql.Date;
 
 public class User {
-	int id;
-	String username;
-	String Password;
-	String email;
-	int pow;
-	String img;
-	String phone;
-	String token;
-	int isNewMsg;
-	Date reg_time;
+	private int id;
+	private String username;
+	private String Password;
+	private String email;
+	private int pow;
+	private String img;
+	private String phone;
+	private String token;
+	private int isNewMsg;
+	private Date reg_time;
 	public int getId() {
 		return id;
 	}
