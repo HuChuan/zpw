e5c228c311420f04449062b0aa569ba3d5c4ab5d
CHEN2333 <1558267774@qq.com>
2016-04-05 23:44:45 +0800

    、

--- WebRoot/js/userspace.js
+++ WebRoot/js/userspace.js
@@ -93,11 +93,112 @@ $(function() {
 
 // 个人简历
 $(function() {
+	loadVitae();
+	$("#nav_cv a").click(function() {
+		loadVitae();
+	});
 	// 添加简历
 	$("#addCV_btn").click(function() {
 		location.href = "setvitae.jsp";
 	});
+});
 
+// 修改密码
+$(function() {
+	$("#psw_btn").click(function() {
+		var old = $("#old_psw").val();
+		var _new = $("#new_psw").val();
+		var re = $("#re_new_psw").val();
+		if (old.length < 6 || _new.length < 6) {
+			setLog("密码长度不能少于6位", false);
+		} else if (_new != re) {
+			setLog("密码不一致", false);
+		} else {
+			$.post("update_password.do", {
+				username : $.cookie("username"),
+				old_psw : old,
+				new_psw : _new
+			}, function(data) {
+				if (data.success == true) {
+					setLog("修改成功");
+					$("#psw_f")[0].reset();
+				} else {
+					setLog(data.reason, false);
+				}
+			}, "json");
+		}
+	});
+});
+
+// 加载已投职位信息
+$(function() {
+	$.post("load_cv_job.do", {
+		username : $.cookie("username")
+	}, function(data) {
+		for (var i = 0; i < data.length; i++) {
+			var read = "";
+			var status = "未读"
+			if (data[i].status == 1) {
+				read = "read";
+				status = "已读";
+			}
+			var $li = $("<li class='ellipsis " + read + "'></li>");
+			var $a = $("<a href='showjob.jsp?id=" + data[i].job_id + "'>"
+					+ data[i].job_name + "</a>");
+			var $span1 = $("<span class='epname'>" + data[i].ep_username
+					+ "</span>");
+			var $span2 = $("<span class='status'>" + status + "</span>");
+			var $span3 = $("<span class='time'>" + formatDate(data[i].time)
+					+ "</span>");
+			$li.append($a);
+			$li.append($span1);
+			$li.append($span2);
+			$li.append($span3);
+			$("#vote_list").append($li);
+		}
+	}, "json");
+});
+
+// 设置header导航激活样式
+$("#header_nav").addClass("personal");
+
+// 消息提示设置
+function setLog(text, error) {
+	$("#log_info").text(text);
+	if (error == false) {
+		$("#log_info").addClass("error");
+	} else {
+		$("#log_info").removeClass("error");
+	}
+	$("#log_info").fadeIn(200);
+	setTimeout(function() {
+		$("#log_info").fadeOut(200);
+	}, 2000);
+}
+
+// 加载用户基本信息
+function loadUserInfo() {
+	$.post("load_userinfo.do", {
+		username : $.cookie("username")
+	}, function(data) {
+		var pow = "个人用户";
+		var f = $("#user_f")[0];
+		if (data.pow == "2") {
+			pow = "企业用户";
+		} else if (data.pow == "3") {
+			pow = "管理员";
+		}
+		f.username.value = data.username;
+		f.pow.value = pow;
+		f.email.value = data.email;
+		f.phone.value = data.phone;
+		$("#user_img").attr("src", data.img);
+		old_email = data.email;
+	}, "json");
+}
+
+// 加载个人简历
+function loadVitae() {
 	// 检查是否存在简历
 	$.post("check_vitae.do", {
 		username : $.cookie("username")
@@ -143,102 +244,4 @@ $(function() {
 			right : "-146px",
 		}, 100);
 	});
-});
-
-// 修改密码
-$(function() {
-	$("#psw_btn").click(function() {
-		var old = $("#old_psw").val();
-		var _new = $("#new_psw").val();
-		var re = $("#re_new_psw").val();
-		if (old.length < 6 || _new.length < 6) {
-			setLog("密码长度不能少于6位", false);
-		} else if (_new != re) {
-			setLog("密码不一致", false);
-		} else {
-			$.post("update_password.do", {
-				username : $.cookie("username"),
-				old_psw : old,
-				new_psw : _new
-			}, function(data) {
-				if (data.success == true) {
-					setLog("修改成功");
-					$("#psw_f")[0].reset();
-				} else {
-					setLog(data.reason, false);
-				}
-			}, "json");
-		}
-	});
-});
-
-// 加载已投职位信息
-$(function() {
-	$
-			.post("load_cv_job.do", {
-				username : $.cookie("username")
-			},
-					function(data) {
-						for (var i = 0; i < data.length; i++) {
-							var read = "";
-							var status = "未读"
-							if (data[i].status == 1) {
-								read = "read";
-								status = "已读";
-							}
-							var $li = $("<li class='ellipsis " + read
-									+ "'></li>");
-							var $a = $("<a href='showjob.jsp?id=" + data[i].job_id + "'>"
-									+ data[i].job_name + "</a>");
-							var $span1 = $("<span class='epname'>"
-									+ data[i].ep_username + "</span>");
-							var $span2 = $("<span class='status'>" + status
-									+ "</span>");
-							var $span3 = $("<span class='time'>" + formatDate(data[i].time)
-									+ "</span>");
-							$li.append($a);
-							$li.append($span1);
-							$li.append($span2);
-							$li.append($span3);
-							$("#vote_list").append($li);
-						}
-					}, "json");
-});
-
-// 设置header导航激活样式
-$("#header_nav").addClass("personal");
-
-// 消息提示设置
-function setLog(text, error) {
-	$("#log_info").text(text);
-	if (error == false) {
-		$("#log_info").addClass("error");
-	} else {
-		$("#log_info").removeClass("error");
-	}
-	$("#log_info").fadeIn(200);
-	setTimeout(function() {
-		$("#log_info").fadeOut(200);
-	}, 2000);
-}
-
-// 加载用户基本信息
-function loadUserInfo() {
-	$.post("load_userinfo.do", {
-		username : $.cookie("username")
-	}, function(data) {
-		var pow = "个人用户";
-		var f = $("#user_f")[0];
-		if (data.pow == "2") {
-			pow = "企业用户";
-		} else if (data.pow == "3") {
-			pow = "管理员";
-		}
-		f.username.value = data.username;
-		f.pow.value = pow;
-		f.email.value = data.email;
-		f.phone.value = data.phone;
-		$("#user_img").attr("src",data.img);
-		old_email = data.email;
-	}, "json");
 }
