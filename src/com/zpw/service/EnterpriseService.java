c565aa5aaaf9a03c2f09294fecc8825c27eabfd9
HuChuan <691523638@qq.com>
2016-04-06 18:50:14 +0800

    真的好了

--- src/com/zpw/service/EnterpriseService.java
+++ src/com/zpw/service/EnterpriseService.java
@@ -1,5 +1,7 @@
 package com.zpw.service;
 
+import java.util.List;
+
 import com.zpw.dao.DAOFactory;
 import com.zpw.dao.IEnterpriseDao;
 import com.zpw.po.Enterprise;
@@ -16,5 +18,9 @@ public class EnterpriseService {
 	public static Enterprise getEnterpriseById(int id){
 		IEnterpriseDao e = DAOFactory.getEnterpriseDao();
 		return e.qById(id);
+	}
+	public static List<Enterprise> getAllEnterprise(){
+		IEnterpriseDao e = DAOFactory.getEnterpriseDao();
+		return e.getAllEnterprise();
 	}
 }
